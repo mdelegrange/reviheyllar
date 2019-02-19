89777eda6e72f32abcebfeb0643f45304bf362b6
matthieu delegrange <mdelegrange@gmail.com>
2019-02-19 11:01:26 +0100

    first page and first password added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def password
+    if params[:attempt].upcase == "166C21241011"
+      render 'password_166C21241011'
+    else
+      flash[:error] = "Mauvais mot de passe, essayez encore..."
+      render 'home'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'password', to: 'pages#password'
+  get 'password-166C21241011', to: 'pages#password_166C21241011'
 end
