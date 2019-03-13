6039e7339b140637ef21857512a9e96a58925a2a
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2019-03-14 00:31:51 +0100

    evolutions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   before_action :set_attempt, only: [ :password, :enigme1_attempt,
-    :enigme2_attempt, :enigme3_attempt, :enigme4_attempt, :enigme5_attempt]
+    :enigme2_attempt, :enigme3_attempt, :enigme4_attempt, :enigme5_attempt,
+    :enigme6_attempt, :enigme7_attempt, :enigme8_attempt]
   def home
   end
 
@@ -17,7 +18,7 @@ class PagesController < ApplicationController
 
   def enigme1_attempt
     if @attempt.upcase == "FAITES TOMBER LES BINOCLARDS"
-      Answer.create!(enigma: 1, content: "Faites tomber les binoclards")
+      Answer.create(enigma: 1, content: "Faites tomber les binoclards")
       redirect_to enigme2_path
     else
       flash_error
@@ -32,7 +33,7 @@ class PagesController < ApplicationController
 
   def enigme2_attempt
     if @attempt.upcase == "HOMME"
-      Answer.create!(enigma: 2, content: "Homme")
+      Answer.create(enigma: 2, content: "Homme")
       redirect_to enigme3_path
     else
       flash_error
@@ -47,9 +48,9 @@ class PagesController < ApplicationController
   end
 
   def enigme3_attempt
-    answers = ["PRENOM", "PRÉNOM","PRENOMS", "PRÉNOMS"]
+    answers = ["CÉCILE", "CECILE"]
     if answers.include?(@attempt.upcase)
-      Answer.create!(enigma: 3, content: "Prénoms")
+      Answer.create(enigma: 3, content: "Cécile")
       redirect_to enigme4_path
     else
       flash_error
@@ -63,9 +64,9 @@ class PagesController < ApplicationController
   end
 
   def enigme4_attempt
-    answers = ["CHAPEAU", "CHAPEAUX"]
+    answers = ["CORINNE"]
     if answers.include?(@attempt.upcase)
-      Answer.create!(enigma: 4, content: "Chapeaux")
+      Answer.create(enigma: 4, content: "Corinne")
       redirect_to enigme5_path
     else
       flash_error
@@ -79,16 +80,67 @@ class PagesController < ApplicationController
   end
 
   def enigme5_attempt
-    if @attempt.upcase == "JULIE"
-      Answer.create!(enigma: 5, content: "Julie")
-      redirect_to bravo_path
+    answers = ["CAROLINE"]
+    if answers.include?(@attempt.upcase)
+      Answer.create(enigma: 5, content: "Caroline")
+      redirect_to enigme6_path
     else
       flash_error
       redirect_to enigme5_path
     end
   end
 
+  def enigme6
+    @answer = Answer.find_by(enigma: 6)
+    go_to_first_unsolved_enigma
+  end
+
+  def enigme6_attempt
+    answers = ["CHAPEAU", "CHAPEAUX"]
+    if answers.include?(@attempt.upcase)
+      Answer.create(enigma: 6, content: "Chapeaux")
+      redirect_to enigme7_path
+    else
+      flash_error
+      redirect_to enigme6_path
+    end
+  end
+
+  def enigme7
+    @answer = Answer.find_by(enigma: 7)
+    go_to_first_unsolved_enigma
+  end
+
+  def enigme7_attempt
+    answers = ["yeux marrons"]
+    if answers.include?(@attempt)
+      Answer.create(enigma: 7, content: "yeux marrons")
+      redirect_to enigme8_path
+    else
+      flash_error
+      redirect_to enigme7_path
+    end
+  end
+
+  def enigme8
+    @answer = Answer.find_by(enigma: 8)
+    go_to_first_unsolved_enigma
+  end
+
+  def enigme8_attempt
+    answers = ["JULIE"]
+    if answers.include?(@attempt.upcase)
+      Answer.create(enigma: 8, content: "Julie")
+      redirect_to bravo_path
+    else
+      flash_error
+      redirect_to enigme8_path
+    end
+  end
+
   def bravo
+    @answer = Answer.find_by(enigma: 8)
+    go_to_first_unsolved_enigma
   end
 
   private
